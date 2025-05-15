fa5aec8958aa177960aa16d488b9fd73c9a7a847
Shawn Chee <aykaychee2@gmail.com>
2025-05-16 01:30:00 +0800

    Allow image domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost', 'submint.vercel.app', 'images.pexels.com','purple-historic-bird-762.mypinata.cloud','encrypted-tbn0.gstatic.com'],
+    domains: ['localhost', 'submint.vercel.app', 'images.pexels.com','purple-historic-bird-762.mypinata.cloud','encrypted-tbn0.gstatic.com', 'suonyyhangpwrbbautkb.supabase.co'],
   },
   eslint: {
     ignoreDuringBuilds: true,
